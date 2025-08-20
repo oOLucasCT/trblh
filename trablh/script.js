8c648d59fec326c01f6a8d404f3b5d629ce185a7
LucasCT <131679596+oOLucasCT@users.noreply.github.com>
2025-08-20 10:09:05 -0300

    Add files via upload

---
+++ trablh/script.js
@@ -0,0 +1,30 @@
+function verificar(){
+  let  usuario = ["Ana","Bruna","Carol"];
+  let senha = ["123", "456", "789"];
+  let input = document.getElementById("loguin").value;
+  let input2 = document.getElementById("senha").value;
+  const frase = document.getElementById("frase");
+  let encontrado = false
+  for (let i = 0; i < usuario.length; i++){
+    if(input == usuario[i]){
+    encontrado = true
+    frase.innerHTML= "senha incorreta"
+    frase.classList.remove("text1")
+    frase.classList.remove("text3")
+    frase.classList.add("text2")
+ }
+    if(input2 == senha[i]){
+      encontrado = true
+      frase.innerHTML= "entrada correta "
+      frase.classList.remove("text1")
+      frase.classList.add("text3")
+    }
+    
+}
+  if(!encontrado){
+  frase.innerHTML= "entrada incorreta"
+   frase.classList.remove("text1")
+   frase.classList.remove("text3")
+   frase.classList.add("text2")
+ }
+}
