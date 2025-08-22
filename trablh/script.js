ebb03a64cd3d030d4f7748cb96d4aaddda23d3a1
LucasCT <131679596+oOLucasCT@users.noreply.github.com>
2025-08-22 11:17:34 -0300

    Add files via upload

--- trablh/script.js
+++ trablh/script.js
@@ -5,26 +5,40 @@ function verificar(){
   let input2 = document.getElementById("senha").value;
   const frase = document.getElementById("frase");
   let encontrado = false
+
   for (let i = 0; i < usuario.length; i++){
     if(input == usuario[i]){
     encontrado = true
-    frase.innerHTML= "senha incorreta"
+    frase.innerHTML= "Senha incorreta"
     frase.classList.remove("text1")
     frase.classList.remove("text3")
     frase.classList.add("text2")
  }
-    if(input2 == senha[i]){
+
+    if(input2 == senha[i] && input == usuario[i]){
       encontrado = true
-      frase.innerHTML= "entrada correta "
+      frase.innerHTML= "Entrada Correta "
       frase.classList.remove("text1")
       frase.classList.add("text3")
+
     }
     
 }
   if(!encontrado){
-  frase.innerHTML= "entrada incorreta"
+  frase.innerHTML= "Entrada incorreta"
    frase.classList.remove("text1")
    frase.classList.remove("text3")
    frase.classList.add("text2")
  }
+ document.getElementById('loguin').value=''
+ document.getElementById('senha').value=''
+}
+
+function mostraSenha() {
+  var x = document.getElementById("senha");
+  if (x.type === "password") {
+    x.type = "text";
+  } else {
+    x.type = "password";
+  }
 }
